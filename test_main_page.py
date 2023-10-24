6dc4a378430554f847c27504f6f45bbd919b29c2
Анна Барахтенко <a.barakhtenko@ispsystem.com>
2023-10-24 21:18:31 +0700

    modified test method

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,12 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)  # page object initialization
+    page.open()                     # open the page
+    page.go_to_login_page()         # call method to go to login page
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
