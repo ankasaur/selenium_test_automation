cca71b933c1c23a08e9d789d06532f6b7f2e6cb9
Анна Барахтенко <a.barakhtenko@ispsystem.com>
2023-12-12 09:22:41 +0700

    removed method for register form test

--- test_login_page.py
+++ test_login_page.py
@@ -1,20 +1,9 @@
 from selenium.webdriver.common.by import By
 from .pages.login_page import LoginPage
 
-def test_guest_should_see_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_url(self)
-
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form(self)
-
-def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form(self)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
